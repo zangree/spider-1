58614872d9d9424fa0521b2aa539ba010dd6d629
mercuryZ <15210820708@163.com>
2018-01-30 13:27:09 +0800

    Merge branch 'master' of github.com:mercuryZ/spider
    update sofang url

--- lianjiascrapy/spiders/fangxiaoquurl.py
+++ lianjiascrapy/spiders/fangxiaoquurl.py
@@ -11,13 +11,6 @@ class LianJiaXiaoQuUrl(scrapy.Spider):
         start_urls = []
         start_domains = [
             ('http://bj.sofang.com/saleesb/area/aa10', 23),
-            # ('https://bj.lianjia.com/xiaoqu/tongzhou/', 22),
-            # ('https://bj.lianjia.com/xiaoqu/changping/', 27),
-            # ('https://bj.lianjia.com/xiaoqu/daxing/', 16),
-            # ('https://bj.lianjia.com/xiaoqu/yizhuangkaifaqu/', 5),
-            # ('https://bj.lianjia.com/xiaoqu/shunyi/', 12),
-            # ('https://bj.lianjia.com/xiaoqu/mentougou/', 8),
-            # ('https://bj.lianjia.com/xiaoqu/miyun/', 5)
         ]
         for item, pages in start_domains:
             for i in range(1, pages):
