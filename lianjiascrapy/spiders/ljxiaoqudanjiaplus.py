724957fe7ca75e07bcc403b520134bc65199a30c
mercuryZ <15210820708@163.com>
2018-05-09 14:56:13 +0800

    add bjxiaoqudanjiaplus

---
+++ lianjiascrapy/spiders/ljxiaoqudanjiaplus.py
@@ -0,0 +1,35 @@
+#-*- coding: utf-8 -*-
+
+import scrapy
+from lianjiascrapy.items import LJXiaoquDanjiaItem
+
+class LJXiaoquDanjiaSpider(scrapy.Spider):
+    name = 'bjxiaoqudanjiaplus'
+    allowed_domains = ['bj.lianjia.com']
+
+    def start_requests(self):
+        start_urls = []
+        start_domains = []
+        xiaoqufilename = ""
+        with open(xiaoqufilename) as f:
+            for item in f:
+                start_domains.append(item)
+
+        for url in start_domains[:10]:
+            start_urls.append(scrapy.Request(url.strip()))
+
+        return start_urls
+
+    def parse(self, response):
+        item = LJXiaoquDanjiaItem()
+        name = response.xpath('//h1[@class="detailTitle"]/text()').extract()
+        if name:
+            item['title'] = name[0].strip()
+        else:
+            item['title'] = '暂无信息'
+        price = response.xpath('//span[@class="xiaoquUnitPrice"]/text()').extract()
+        if price:
+            item['price'] = price[0].strip()
+        else:
+            item['price'] = '暂无信息'
+        yield item
